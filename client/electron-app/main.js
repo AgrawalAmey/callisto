a78f359885e85543dad0aa9b037433f6100e7ae4
Amey Agrawal <agrawalamey12@gmail.com>
2018-01-14 21:35:30 +0530

    blah

--- client/electron-app/main.js
+++ client/electron-app/main.js
@@ -12,39 +12,38 @@ const BrowserWindow = electron.BrowserWindow;
 // Load config file
 const config = require('./config');
 const remoteServerAddr = 'http://' + config.remoteServer.host + ':' + config.remoteServer.port;
-
+const jupyterAddr = 'http://' + config.jupyter.host + ':' + config.jupyter.port;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600});
 
-  mainWindow.maximize()
+  mainWindow.maximize();
 
-  request.get(remoteServerAddr, function(err, body, res) {
+  request.get(remoteServerAddr+"/login", function(err, body, res) {
     if(err) {
       console.log(err);
       mainWindow.loadURL(path.join('file://', __dirname, 'views', 'remoteAddr.ejs'));
     } else {
-      ejse.data('remoteServerAddr', remoteServerAddr);
+      ejse.data('remoteServerAddr', "http://localhost:19350/notebooks/grabage_collection/Simple%20pdf%20model.ipynb");
       mainWindow.loadURL(path.join('file://', __dirname, 'views', 'webview.ejs'));
-
     }
   });
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
@@ -57,7 +56,7 @@ app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
@@ -65,7 +64,7 @@ app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
 })
 
