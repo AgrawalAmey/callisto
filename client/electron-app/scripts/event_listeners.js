3892fe0ee4aa3f884f8dc3952a2fd0c1c47c72f2
Shrikant Sharda <shrikant.sharda@gmail.com>
2018-01-16 00:57:09 +0530

    Added event handlers for user login and logout

--- client/electron-app/scripts/event_listeners.js
+++ client/electron-app/scripts/event_listeners.js
@@ -1,11 +1,17 @@
 // Load vendor scripts
 const electron = require('electron');
 
+// Load app instance
+const app = electron.remote;
+
 // Load custom scripts
 const helpers = require('./helpers.js');
 
 // Module to communicate with UI
 const ipc = electron.ipcMain;
+
+// Utility functions
+const utils = require('./utils');
 
 module.exports = {
     setEventListerns: function (mainWindow){
@@ -18,6 +24,26 @@ module.exports = {
         ipc.on('test', function (event, data) {
             console.log("yooooooooooooooo");
             console.log(data);
+        });
+
+        // Delete user credentials on logout
+        ipc.on('removeUserCreds', function () {
+            utils.removeCreds();
+        });
+
+        // Delete user credentials on closing all windows of app
+        app.on('window-all-closed', function() {
+            utils.removeCreds();
+        });
+
+        // Delete user credentials on closing main window
+        mainWindow.on('closed', function() {
+            utils.removeCreds();
+        });
+
+        // Save user credentials on login
+        ipc.on('saveUserCreds', function (event, userCreds) {
+            utils.saveCreds(userCreds.username, userCreds.password);
         });
     }
 }
